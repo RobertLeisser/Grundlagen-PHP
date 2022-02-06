c1d1be6496d83e81d0b51c6b14d180394affee00
RobertLeisser <80169533+RobertLeisser@users.noreply.github.com>
2022-02-06 12:09:45 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,7 +1,54 @@
 <?php
-class Goodbye {
-  const LEAVING_MESSAGE = "Thank you for visiting W3Schools.com!";
+
+session_start();
+
+require_once(__DIR__."/../config.php");
+require_once(__DIR__."/UrlShortener.php");
+
+$errors       = false;
+$insertCustom = false;
+
+$urlShortener = new UrlShortener();
+
+if (($_POST['onoffswitch'] == 'on') && (isset($_POST['custom']))) {
+    $customCode = $_POST['custom'];
+    
+    if (!$urlShortener->checkUrlExistInDatabase($customCode)) {
+        $insertCustom = true;
+    }
+    
+    else {
+        $errors            = true;
+        $_SESSION['error'] = 'The custom URL <a href="' . BASE_URL . $_POST['custom'] . '">' . BASE_URL . $_POST['custom'] . "</a> already exists";
+    }
 }
 
-echo Goodbye::LEAVING_MESSAGE;
+if (isset($_POST['url']) && !$errors) {
+    $orignalURL = $_POST['url'];
+    
+    if (!$insertCustom) {
+        if ($uniqueCode = $urlShortener->validateUrlAndReturnCode($orignalURL)) {
+            $_SESSION['success'] = $urlShortener->generateLinkForShortURL($uniqueCode);
+        }
+        
+        else {
+            $_SESSION['error'] = "There was a problem. Invalid URL, perhaps?";
+        }
+    }
+    
+    else {
+        if ($urlShortener->returnCustomCode($orignalURL, $customCode)) {
+            $_SESSION['success'] = $urlShortener->generateLinkForShortURL($customCode);
+        }
+        
+        else {
+            header("Location: ../index.php?error=inurl");
+            die();
+        }
+    }
+}
+
+header("Location: ../index.php");
+exit();
+
 ?>
